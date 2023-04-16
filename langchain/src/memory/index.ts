10436a409b5e0a72edae624d710005256036592b
soylomass <federicomouse@gmail.com>
2023-04-16 20:22:18 -0300

    ConversationSummaryBufferMemory

--- langchain/src/memory/index.ts
+++ langchain/src/memory/index.ts
@@ -1,6 +1,7 @@
 export { BufferMemory } from "./buffer_memory.js";
 export { BaseMemory } from "./base.js";
 export { ConversationSummaryMemory } from "./summary.js";
+export { ConversationSummaryBufferMemory } from "./summary_buffer.js";
 export { BufferWindowMemory } from "./buffer_window_memory.js";
 export { BaseChatMemory, ChatMessageHistory } from "./chat_memory.js";
 export { MotorheadMemory } from "./motorhead_memory.js";
